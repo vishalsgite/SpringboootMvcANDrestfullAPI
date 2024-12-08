fd96e2f43414232a2bc39db027db3627edbf7776
vishalsgite <vshlsgite.98.vg@gmail.com>
2024-12-08 12:37:37 +0530

    Spring MVC and Controller ,Annotation RequestMapping

--- springbootwebtutorial/src/main/java/com/vishal/springbootwebtutorial/springbootwebtutorial/Controllers/EmployeeController.java
+++ springbootwebtutorial/src/main/java/com/vishal/springbootwebtutorial/springbootwebtutorial/Controllers/EmployeeController.java
@@ -3,13 +3,12 @@ package com.vishal.springbootwebtutorial.springbootwebtutorial.Controllers;
 
 import com.vishal.springbootwebtutorial.springbootwebtutorial.Entities.EmployeeDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/ParentPathEMPLOYEES")
 public class EmployeeController {
 
 
@@ -23,5 +22,29 @@ public class EmployeeController {
         return new EmployeeDTO(employeeID ,
                 "VISHAL","VSHL@GMAIL.COM",26,
                 LocalDate.of(2024,8,5),true);
+    }
+
+    @GetMapping(path = "/getEmpByAddingNameParaInAbove/{employeeID}")
+    public EmployeeDTO getEmployeeByIDWithName(@PathVariable(name = "employeeID") Long ID){
+        return new EmployeeDTO(ID ,
+                "VISHAL","VSHL@GMAIL.COM",26,
+                LocalDate.of(2024,8,5),true);
+    }
+
+   // @GetMapping(path = "/employee")
+    @GetMapping
+    public String getAllemployees(@RequestParam(required = false) Integer age,
+                                  @RequestParam(required = false) String sortBy){
+        return "Hi filter by age is : "+age+" "+sortBy;
+
+
+    }
+
+    @GetMapping(path = "/getAllemployeesByMyAgePara")
+    public String getAllemployeesByMyAgePara(@RequestParam(required = false,name = "MyAge") Integer age,
+                                  @RequestParam(required = false) String sortBy){
+        return "Hi filter by age is : "+age+" "+sortBy;
+
+
     }
 }
